4d4fff018d12907409613a40f642ef9eae32cd8d
Malik <malik.h@webdevpro.net>
2022-11-21 14:37:07 +0100

    cas pratique

--- jour1-passport/routes.js
+++ jour1-passport/routes.js
@@ -1,6 +1,7 @@
 const {Router} = require("express");
 const  { User } = require("./model-user");
 const bcrypt = require("bcrypt");
+const Joi = require("joi");
 
 const route = Router();
 
@@ -18,8 +19,17 @@ route.get("/inscrire" , (req, rep) => {
     rep.send(formulaire)
 })
 route.post("/inscrire" , async (req, rep) => {
+    const schemaUser = Joi.object({
+        email : Joi.string().email({ tlds : {allow :false} }).required(),
+        password : Joi.string().min(5).required()
+    })
 
     try{
+
+        const {error} = schemaUser.validate(req.body , { abortEarly : false })
+
+        if(error) return rep.status(401).json(error)
+
         const salt = await bcrypt.genSalt(10); // géréner un salt token qui permet de hashed le mot de passe
         const passwordHashed = await bcrypt.hash( req.body.password , salt ) ;
 
