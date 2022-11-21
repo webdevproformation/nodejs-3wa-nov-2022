fa39c1242841dea516a12abe1f2709a0ef612521
Malik <malik.h@webdevpro.net>
2022-11-21 10:55:36 +0100

    correction

--- jour1-session/index.js
+++ jour1-session/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")
 
 const app = express()
 const PORT = 5000 ;
@@ -8,6 +10,21 @@ const PORT = 5000 ;
 mongoose.connect( process.env.BDD , {useNewUrlParser : true} )
         .then( () => console.log("connexion reussie à la base de données") )
         .catch((ex) => console.log("erreur lors de la connexion à la bdd" , ex))
+
+
+app.use(session({
+    secret : "azerty1234!",
+    resave : false ,
+    saveUninitialized : false ,
+    store : MongoStore.create({
+        mongoUrl : process.env.BDD,
+        collectionName : "sessions"
+    }) ,
+    cookie : {
+        maxAge: 60 * 60 * 1000
+    }
+}))
+
 
 app.get("/" , (req, rep) => {
     rep.json({ message : "welcome" })
