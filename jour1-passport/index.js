73eadee2103d2fbf234ed8234dc71fc60c4308f2
Malik <malik.h@webdevpro.net>
2022-11-21 12:13:18 +0100

    cas pratique

---
+++ jour1-passport/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+require("dotenv").config();
+const PORT = 5002;
+
+const app = express();
+
+app.use(session({
+    secret : "secret",
+    resave : false ,
+    saveUninitialized : false ,
+    cookie : {
+        maxAge : 1000 * 60* 60
+    },
+    store : MongoStore.create({
+        mongoUrl : process.env.BDD,
+        collection : "sessions"
+    })
+}))
+
+app.listen(PORT, function(){
+    console.log(`connexion express réussie sur le port ${PORT}`)
+})
+
