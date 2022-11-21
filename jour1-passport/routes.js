0d0ee2d298b3cf7f5021be372663ace89cad5fe6
Malik <malik.h@webdevpro.net>
2022-11-21 15:00:35 +0100

    utilisation de passport sur la route post de connexion

--- jour1-passport/routes.js
+++ jour1-passport/routes.js
@@ -2,6 +2,7 @@ const {Router} = require("express");
 const  { User } = require("./model-user");
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
+const passport = require("passport")
 
 const route = Router();
 
@@ -28,7 +29,13 @@ route.post("/inscrire" , async (req, rep) => {
 
         const {error} = schemaUser.validate(req.body , { abortEarly : false })
 
-        if(error) return rep.status(401).json(error)
+        if(error) return rep.status(400).json(error)
+
+        /**vérifier que l'email n'existe pas déjà en base de donnée */
+
+        const userDoublon = await User.findOne({email : req.body.email})
+
+        if(userDoublon) return rep.status(400).json({message : `l'email ${req.body.email} est déjà utilisé`}); 
 
         const salt = await bcrypt.genSalt(10); // géréner un salt token qui permet de hashed le mot de passe
         const passwordHashed = await bcrypt.hash( req.body.password , salt ) ;
@@ -40,8 +47,8 @@ route.post("/inscrire" , async (req, rep) => {
         }
 
         const nouveauProfilUser = new User(profilUser)
-        const user = await nouveauProfilUser.save()
-        console.log(user);
+        await nouveauProfilUser.save()
+       
         rep.redirect("/connecter")
     }
     catch(ex){
@@ -52,16 +59,29 @@ route.post("/inscrire" , async (req, rep) => {
 route.get("/connecter" , (req, rep) => {
     const formulaire = `
         <h1>connexion</h1>
-        <form method="POST" action="/inscrire">
+        <form method="POST" action="/connecter">
             <input type="email" name="email" placeholder="votre@email.fr">
             <br>
             <input type="password" name="password" placeholder="votre password">
             <br>
-            <input type="submit" value="créer un nouveau profil">   
+            <input type="submit" value="se connecter">   
         </form>
     `;
     rep.send(formulaire)
 })
+
+/* route.post("/connecter" , () => {
+    //...
+}) */
+
+route.post("/connecter" , passport.authenticate('local' , 
+// si on est on a bien saisit des indentifiants correctes et qui existent en base de donnée 
+// sinon si c'est KO
+{
+    successRedirect : "/admin",
+    failureRedirect : "/connecter"
+}))
+
 route.get("/admin" , (req, rep) => {
     rep.send(
         `<h1>Back Office</h1>
