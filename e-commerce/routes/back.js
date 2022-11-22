041f14ca03c8534e0364f4bfa32ef1605197d5b5
Malik <malik.h@webdevpro.net>
2022-11-22 16:19:34 +0100

    correction

--- e-commerce/routes/back.js
+++ e-commerce/routes/back.js
@@ -15,7 +15,7 @@ router.get("/catalogue", async (req, rep) => {
 })
 
 router.get("/catalogue/new", (req, rep) => {
-    rep.render("back/catalogue/form");
+    rep.render("back/catalogue/form",{titre : "créer un nouveau produit"});
 })
 
 router.post("/catalogue/new", async (req, rep) => {
@@ -34,6 +34,36 @@ router.post("/catalogue/new", async (req, rep) => {
         rep.redirect("/page-erreur")
     }
 })
+
+router.get("/catalogue/update/:id", async (req, rep) => {
+    const id = req.params.id ;
+    if( !isValidObjectId(id) ) return rep.status(400).json({message : "id invalid"})
+    const produit = await Produit.findById(id);
+    if(!produit) return rep.status(404).json({message : "produit inconnu"});
+    rep.render("back/catalogue/form-update" , {produit , titre : "mettre à jour le produit"});
+})
+
+router.post("/catalogue/update" , async (req, rep) => {
+    req.body.en_stock = req.body.en_stock === "1" ? true : false ;
+    //return rep.json({data : req.body})
+    if( !isValidObjectId(req.body.id) ) return rep.status(400).json({message : "id invalid"})
+    const produit = await Produit.findById(req.body.id);
+    if(!produit) return rep.status(404).json({message : "produit inconnu"});
+
+    delete req.body.id ;
+
+    const {error} = validationProduit.validate(req.body)
+    if(error) return rep.json({message : error})
+    produit.nom = req.body.nom
+    produit.prix = req.body.prix
+    produit.description = req.body.description
+    produit.en_stock = req.body.en_stock
+    produit.image = req.body.image
+    await produit.save();
+    rep.redirect("/admin/catalogue"); 
+
+})
+
 
 router.delete("/catalogue/:id" , async (req, rep) => {
     const id = req.params.id ;
