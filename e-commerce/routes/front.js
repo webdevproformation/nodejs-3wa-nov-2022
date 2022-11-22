77b78a9219d11b4335e7df42c410f7e2f5ac5f6b
Malik <malik.h@webdevpro.net>
2022-11-22 14:35:22 +0100

    afficher la liste des produits en stock dans le front

--- e-commerce/routes/front.js
+++ e-commerce/routes/front.js
@@ -1,9 +1,11 @@
 const { Router } = require("express")
+const { Produit } = require("../models/produits")
 
 const router = Router();
 
-router.get("/", (req, rep) => {
-    rep.render("front/index");
+router.get("/", async (req, rep) => {
+    const produits = await Produit.find({en_stock : true})
+    rep.render("front/index" , { produits });
 })
 
 
