e43029f6aa0862b5eb7a6cb17568a56031d4e84a
Malik <malik.h@webdevpro.net>
2022-11-25 10:48:47 +0100

    correction

--- e-commerce/routes/back-users.js
+++ e-commerce/routes/back-users.js
@@ -23,6 +23,7 @@ router.delete("/user/:id", async(req, rep) => {
 
     if(!user) return rep.status(404).json({message : "aucun user trouvé"})
 
+
     // si l'utilisateur est un client de base => role === "client"
     // le supprimer ET supprimer toutes les commandes associées cet utilisateur
 
@@ -30,7 +31,7 @@ router.delete("/user/:id", async(req, rep) => {
         // rechercher tous les commandes réalisées par ce user 
         try{
             // supprimer le compte user 
-            await User.deleteOne({id : user._id})
+            await User.deleteOne({_id : user._id})
             // supprimer tous les commandes associées 
             await Commande.deleteMany({"client.email" : user.email})
             return rep.json({message : "ok"})
@@ -46,7 +47,7 @@ router.delete("/user/:id", async(req, rep) => {
        if(userAdmin.length > 1){
         if(id !== req.session.passport.user._id){
            // return rep.json({message : id}); 
-            await User.deleteOne({id})
+            await User.deleteOne({_id : user._id})
             return rep.json({message : "ok"})
         }
         return rep.status(400).json({message : "un admin ne peut pas s'auto supprimé"})
