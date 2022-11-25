54e5e191e5604e85b48d21fa6eb8cf33ec6b2bbc
Malik <malik.h@webdevpro.net>
2022-11-25 12:48:19 +0100

    page de traitement de la commande

--- e-commerce/routes/back-commande.js
+++ e-commerce/routes/back-commande.js
@@ -1,5 +1,6 @@
 const { Router } = require("express")
 const { Commande } = require("../models/commande")
+const {isValidObjectId} = require("mongoose")
 
 const router = Router();
 
@@ -9,6 +10,19 @@ router.get("/commande" , async (req, rep) => {
         rep.render( "back/commande/index" , { commandes , session: req.session})
     }catch(ex){
         rep.status(500).json({message : "impossible de récupérer les commandes"})
+    }  
+})
+
+router.get("/commande/update/:id" ,  async (req, rep) => {
+    const id = req.params.id ;
+    if( ! isValidObjectId(id)) return rep.status(400).json({message : "id commande invalide"})
+    try{
+        const commande = await Commande.findById(id)
+        const titre = `gestion de la commande ${id}`;
+        const total = commande.produits.reduce( (cumul , item) => {return cumul + item.quantite * item.prix } , 0)
+        rep.render( "back/commande/form-update" , { commande , titre , total , session: req.session})
+    }catch(ex){
+        rep.status(500).json({message : "impossible d'update la commande "})
     }  
 })
 
